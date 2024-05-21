acbbdffe64da69cdd201d2d4593f7c5686e8a654
durlav <thedurlav@gmail.com>
2024-05-22 00:21:57 +0530

    final deploy crud

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ function App() {
   const [tasks, setTasks] = useState([]);
   async function handleAddTask(task) {
     try {
-      let response = await axios.post(
-        "http://localhost:8080/todo/addTask",
-        task,
-        { headers: { "Content-Type": "application/json" } }
-      );
       // let response = await axios.post(
-      //   "https://todoapp-production-11b7.up.railway.app/todo/addTask",
+      //   "http://localhost:8080/todo/addTask",
       //   task,
       //   { headers: { "Content-Type": "application/json" } }
       // );
+      let response = await axios.post(
+        "https://todoapp-production-11b7.up.railway.app/todo/addTask",
+        task,
+        { headers: { "Content-Type": "application/json" } }
+      );
       let data = await response.data;
       console.log(data);
       setTasks((prevTasks) => [...prevTasks, data]);
@@ -30,8 +30,12 @@ function App() {
     let confirmation = window.confirm("You want to delete?");
     if (confirmation) {
       try {
+        // let response = await axios.delete(
+        //   "http://localhost:8080/todo/deleteTask",
+        //   { headers: { id: id } }
+        // );
         let response = await axios.delete(
-          "http://localhost:8080/todo/deleteTask",
+          "https://todoapp-production-11b7.up.railway.app/todo/deleteTask",
           { headers: { id: id } }
         );
         let data = await response.data;
@@ -49,15 +53,20 @@ function App() {
           id,
         },
       };
+      // let response = await axios.put(
+      //   "http://localhost:8080/todo/updateTask",
+      //   null,
+      //   config
+      // );
       let response = await axios.put(
-        "http://localhost:8080/todo/updateTask",
+        "https://todoapp-production-11b7.up.railway.app/todo/updateTask",
         null,
         config
       );
       let data = await response.data;
       console.log(data);
       setTasks((prevTasks) =>
-        prevTasks.map((task) => (task.id == id ? data : task))
+        prevTasks.map((task) => (task.id === id ? data : task))
       );
     } catch (error) {
       console.log(error);
